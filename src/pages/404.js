b712c1d28bcce948dd24297c8483960001a93a11
Sandr <sandrajakobsson81@gmail.com>
2024-01-17 15:12:51 +0100

    Lagt till 404 info

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { Link } from "gatsby";
 
 const NotFound = () => {
   return (
@@ -7,6 +8,16 @@ const NotFound = () => {
       <div>
         <h2>404</h2>
         <p>Sorry, that page doesn't exist (yet)!</p>
+        <Link
+          to="/"
+          style={{
+            textDecoration: "none",
+            color: "#b98c04",
+            fontWeight: "bold",
+          }}
+        >
+          Tillbaka hem
+        </Link>
       </div>
     </Layout>
   );
