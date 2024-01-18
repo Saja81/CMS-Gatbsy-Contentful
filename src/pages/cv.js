649b20e400195a27c05adc9808b0de16d07bbd39
Sandr <sandrajakobsson81@gmail.com>
2024-01-18 18:53:19 +0100

    uppdaterat cv med header/footer

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { graphql, Link } from "gatsby";
 // import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-// import Layout from "../components/Layout";
+import Layout from "../components/Layout";
 import CvHeaderSection from "../components/CvHeaderComponent";
 import CvContactSection from "../components/CvContactComponent";
 import CvWorkSection from "../components/CvWorkComponent";
@@ -23,43 +23,43 @@ const CvHeader = ({ data }) => {
   const kompetenserNodes = data.kompetenser.nodes;
 
   return (
-    // <Layout>
-    <section id={cvMainContainer}>
-      <Helmet>
-        <title>CV - Frontendutvecklare</title>
-        <meta
-          name="description"
-          content="Utforska mitt CV och mina erfarenheter inom administration, reception, ledarskap och frontendutveckling. Jag har tidigare arbetat som driftsamordnare, administratör, receptionist och föreningssekreterare. Mina kompetenser inkluderar administration, reception, ledarskap, BRP Systems, samordning, coaching, evenemangsplanering och journalsystemet Webdoc."
-        />
-      </Helmet>
-      <div id={cvHeader}>
-        <Link to="/">
-          <p>Tillbaka hem</p>
-        </Link>
-        <CvHeaderSection kontakt={kontakt} />
-      </div>
-      <div>
-        <GatsbyImage
-          className={profileImage}
-          image={getImage(kontakt.image.gatsbyImageData)}
-          alt={kontakt.image.title || ""}
-          style={{ maxWidth: "50%" }}
-        />
-      </div>
-      <div id={cvMain}>
-        <div id={cvAside}>
-          <CvContactSection
-            kontakt={kontakt}
-            kompetenserNodes={kompetenserNodes}
+    <Layout>
+      <section id={cvMainContainer}>
+        <Helmet>
+          <title>CV - Frontendutvecklare</title>
+          <meta
+            name="description"
+            content="Utforska mitt CV och mina erfarenheter inom administration, reception, ledarskap och frontendutveckling. Jag har tidigare arbetat som driftsamordnare, administratör, receptionist och föreningssekreterare. Mina kompetenser inkluderar administration, reception, ledarskap, BRP Systems, samordning, coaching, evenemangsplanering och journalsystemet Webdoc."
+          />
+        </Helmet>
+        <div id={cvHeader}>
+          <Link to="/">
+            <p>Tillbaka hem</p>
+          </Link>
+          <CvHeaderSection kontakt={kontakt} />
+        </div>
+        <div>
+          <GatsbyImage
+            className={profileImage}
+            image={getImage(kontakt.image.gatsbyImageData)}
+            alt={kontakt.image.title || ""}
+            style={{ maxWidth: "50%" }}
           />
         </div>
-        <div id={cvArticles}>
-          <CvWorkSection data={data} />
-          <CvEducationSection data={data} />
+        <div id={cvMain}>
+          <div id={cvAside}>
+            <CvContactSection
+              kontakt={kontakt}
+              kompetenserNodes={kompetenserNodes}
+            />
+          </div>
+          <div id={cvArticles}>
+            <CvWorkSection data={data} />
+            <CvEducationSection data={data} />
+          </div>
         </div>
-      </div>
-    </section>
-    // </Layout>
+      </section>
+    </Layout>
   );
 };
 
