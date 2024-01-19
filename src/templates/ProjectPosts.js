b2b57ff1448d709b7181c2d269641a25c5ec25df
Sandr <sandrajakobsson81@gmail.com>
2024-01-19 07:58:08 +0100

    SMå justeringar, template projekt, interaktion länkar nav, färger.

--- src/templates/ProjectPosts.js
+++ src/templates/ProjectPosts.js
@@ -1,7 +1,7 @@
 // ProjectPosts.js
 import React from "react";
 import { graphql } from "gatsby";
-import { card, projTitle } from "../styles/projekt.module.css";
+import { card, projTitle, templateMain } from "../styles/projekt.module.css";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import Layout from "../components/Layout";
 
@@ -9,38 +9,40 @@ const Project = ({ data }) => {
   const project = data.contentfulProjekt;
   return (
     <Layout>
-      <div className={card}>
-        <h3 className={projTitle}>{project.titel}</h3>
-        <p>{project.subtitel}</p>
+      <main id={templateMain}>
+        <div className={card}>
+          <h3 className={projTitle}>{project.titel}</h3>
+          <p>{project.subtitel}</p>
 
-        <div>
-          <img
-            src={project.image1.file.url}
-            alt={project.namn}
-            style={{ width: "100px", height: "100px" }}
-          />
-          {project.image2 && project.image2.file && (
+          <div>
             <img
-              src={project.image2.file.url}
+              src={project.image1.file.url}
               alt={project.namn}
               style={{ width: "100px", height: "100px" }}
             />
+            {project.image2 && project.image2.file && (
+              <img
+                src={project.image2.file.url}
+                alt={project.namn}
+                style={{ width: "100px", height: "100px" }}
+              />
+            )}
+          </div>
+          <a
+            href={project.lnk}
+            target="_blank"
+            rel="noopener noreferrer"
+            // style={{ display: "none" }}
+          >
+            {project.lnk}
+          </a>
+          {project.beskrivning && project.beskrivning.raw && (
+            <div>
+              {documentToReactComponents(JSON.parse(project.beskrivning.raw))}
+            </div>
           )}
         </div>
-        <a
-          href={project.lnk}
-          target="_blank"
-          rel="noopener noreferrer"
-          // style={{ display: "none" }}
-        >
-          {project.lnk}
-        </a>
-        {project.beskrivning && project.beskrivning.raw && (
-          <div>
-            {documentToReactComponents(JSON.parse(project.beskrivning.raw))}
-          </div>
-        )}
-      </div>
+      </main>
     </Layout>
   );
 };
